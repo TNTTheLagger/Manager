92b4edea4e74b23e4f90e59e5157a887de87b89b
TNTTheLagger <tntcsatornaja@gmail.com>
2023-06-01 12:37:28 +0300

    Fixed some stuff here and there!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var mysql = require('mysql');
+const fetch = require('node-fetch');
 const util = require('util');
 const body_parser = require('body-parser');
 //var privateKey  = fs.readFileSync('/sslcert/server.key', 'utf8');
@@ -25,6 +26,7 @@ var con = {
 };
 
 function setUpDB(){
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - Setting up DB");
     sqlQuerry("CREATE TABLE IF NOT EXISTS tasksdb.tasks (id int(11) NOT NULL, name varchar(50) DEFAULT NULL, details varchar(255) DEFAULT NULL, isCompleted bool DEFAULT NULL, date datetime DEFAULT NULL, PRIMARY KEY (id));");
 }
 
@@ -39,57 +41,88 @@ async function sqlQuerry(querry){
 
 //var httpsServer = https.createServer(credentials, app);
 app.get('/', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
 app.get('/authors', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/html/authors.html'));
 });
 
 app.get('/calendar', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/html/calendar.html'));
 });
 
 app.get('/img/hatter', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/img/hatter.png'));
 });
 
 app.get('/js/main', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/js/main.js'));
 });
 
 app.get('/css/style', function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     res.sendFile(path.join(__dirname + '/css/style.css'));
 });
 
+
+// app.get('/data/node', function(req, res) {
+//     console.log(req.originalUrl);
+//     res.sendFile(path.join(__dirname + '/node_modules.zip'));
+// });
+
 app.post('/api/addTask', async function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     var name = req.query.name;
     var details = req.query.details;
     var isCompleted = req.query.isCompleted;
     var date = req.query.date;
-    var querry = "INSERT INTO tasksdb.tasks (name, details, isCompleted, date) VALUES ('"+name+"', '"+details+"', '"+isCompleted+"', '"+date+"');";
+    var querry = "INSERT INTO tasksdb.tasks (name, details, inProgress, date) VALUES ('"+name+"', '"+details+"', '"+isCompleted+"', '"+date+"');";
     var result = await sqlQuerry(querry);
     res.send(result);
 });
 
 app.get('/api/getTasks', async function(req, res) {
-    console.log(req.originalUrl);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - "+req.originalUrl);
     var querry = "SELECT * FROM tasks;";
     var result = await sqlQuerry(querry);
-    console.log(result);
     res.send(result);
 });
 
+async function cleanCalendar() {
+    var querry = "SELECT * FROM tasks;";
+    var result = await sqlQuerry(querry);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - Cleaning db");
+    for (var i = 0; i < result.length; i++) {
+        if (Date(result[i].date).toLocaleDateString === new Date().toLocaleDateString) {
+            var querry = "UPDATE tasksdb.tasks SET inProgress = 1 WHERE id = " + result[i].id + ";";
+            var result = await sqlQuerry(querry);
+        }else{
+            await sqlQuerry("UPDATE tasksdb.tasks SET inProgress = 0 WHERE id = " + result[i].id + ";");
+        }
+        if (result[i].date < new Date()) {
+            await sqlQuerry("DELETE FROM tasksdb.tasks WHERE id = " + result[i].id + ";");
+        }
+    }
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - Cleaning done!");
+    setTimeout(cleanCalendar, 60000);
+}
+
+app.get('*',async function(req, res){
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - 404 ERROR - "+req.originalUrl);
+    res.status(404).sendFile(path.join(__dirname + '/img/404.png'));
+});
+
 httpServer.listen(8080, () => {
+    cleanCalendar();
     setUpDB();
-    console.log("server starting on port : http://localhost:" + 8080);
+    setTimeout(cleanCalendar, 60000);
+    console.log("["+new Date().toLocaleDateString()+" " +new Date().toLocaleTimeString()+"] - server starting on port : http://localhost:" + 8080);
 });
 // httpsServer.listen(443, () => {
 // console.log("server starting on port : " + 443)
--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ async function getTasksIndex(){
     var DONT = document.getElementById("DONT");
     for (var i = 0; i < tasks.length; i++){
         var template = document.getElementById("INP").cloneNode(true);
-        console.log(tasks[i].isCompleted);
+        console.log(tasks[i].inProgress);
         if(tasks[i].isCompleted === 1){
             template.classList.add("CELL");
             template.id = tasks[i].id;
@@ -55,4 +55,34 @@ async function getTasksIndex(){
         }
 
     }
+}
+
+async function getCalendarPlan(){
+  var tasks = await getTasks();
+  tasks.sort(function(a, b){
+    var dateA=new Date(a.date), dateB=new Date(b.date)
+    return dateA-dateB
+  })
+  for (var i = 0; i < tasks.length; i++){
+    //tasks[i].date = tasks[i].date.split("T")[0];
+    console.log(tasks[i].date);
+    var tdate=new Date(tasks[i].date)
+    var calendar_events = document.getElementById("calendar_eve").childNodes[3].cloneNode(true);
+    if(tdate > new Date()){
+      console.log(calendar_events.childNodes);
+      if(tasks[i].inProgress === 1){
+        calendar_events.classList.add("event_item_active");
+        console.log(calendar_events.childNodes);
+        calendar_events.childNodes[1].innerHTML = tdate.getDate() + "/" + (tdate.getMonth()+1) + "/" + tdate.getFullYear() + " " + tdate.getHours() + ":" + tdate.getMinutes() + ":" + tdate.getSeconds() + " - " + tasks[i].name;
+        calendar_events.childNodes[3].innerHTML = tasks[i].details;  
+        document.getElementById("calendar_eve").appendChild(calendar_events);
+      }else{
+        console.log(calendar_events.childNodes);
+        calendar_events.childNodes[1].innerHTML = tasks[i].details;
+        calendar_events.childNodes[3].innerHTML = tdate.getDate() + "/" + (tdate.getMonth()+1) + "/" + tdate.getFullYear() + " " + tdate.getHours() + ":" + tdate.getMinutes() + ":" + tdate.getSeconds() + " - " + tasks[i].name;
+        document.getElementById("calendar_eve").appendChild(calendar_events);
+      }
+    }
+  }
+  document.getElementById("calendar_eve").childNodes[3].remove(); 
 }
